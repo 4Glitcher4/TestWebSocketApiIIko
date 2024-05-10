9748f396e3063a11337676f83099a314a4d01829
4Glitcher4 <n.black@gosu.kz>
2024-05-10 11:57:50 +0500

    Add project files.

---
+++ TestWebSocketApiIIko/Controllers/WebsocketController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using TestWebSocketApiIIko.Services;
+
+namespace TestWebSocketApiIIko.Controllers
+{
+    public class WebsocketController : ControllerBase
+    {
+        [HttpGet("websocket")]
+        public async Task Get()
+        {
+            try
+            {
+                if (HttpContext.WebSockets.IsWebSocketRequest)
+                {
+                    using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+                    await Echo(webSocket, "");
+                }
+                else
+                {
+                    HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                HttpContext.Response.WriteAsJsonAsync(ex.Message);
+            }
+        }
+
+        private static async Task Echo(WebSocket webSocket, string data)
+        {
+            try
+            {
+                var ser = new ResponseService();
+
+                var buffer = new byte[1024 * 4];
+                var receiveResult = await webSocket.ReceiveAsync(
+                    new ArraySegment<byte>(buffer), CancellationToken.None);
+
+
+                while (!receiveResult.CloseStatus.HasValue)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+                    var requestDataDict = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(message);
+
+                    byte[] responseData = [];
+                    switch (requestDataDict["command"].ToString())
+                    {
+                        case "Login":
+                            responseData = Encoding.UTF8.GetBytes(ser.Login(requestDataDict["data"]));
+                            break;
+                        case "Sale":
+                            responseData = Encoding.UTF8.GetBytes(ser.Sale(requestDataDict["data"]));
+                            break;
+                        case "Refund":
+                            responseData = Encoding.UTF8.GetBytes(ser.Refund(requestDataDict["data"]));
+                            break;
+                        case "AddPayment":
+                            responseData = Encoding.UTF8.GetBytes(ser.AddPayment(requestDataDict["data"]));
+                            break;
+                        case "ForceReceiptUpload":
+                            responseData = Encoding.UTF8.GetBytes(ser.ForceReceiptUpload(requestDataDict["data"]));
+                            break;
+                        case "CloseZReport":
+                            responseData = Encoding.UTF8.GetBytes(ser.ZReport(requestDataDict["data"]));
+                            break;
+                        case "PrintXReport":
+                            responseData = Encoding.UTF8.GetBytes(ser.XReport(requestDataDict["data"]));
+                            break;
+                        case "PrintLastReceipt":
+                            responseData = Encoding.UTF8.GetBytes(ser.PrintLastReceipt(requestDataDict["data"]));
+                            break;
+                        default:
+                            //HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                            break;
+                    }
+
+                    await webSocket.SendAsync(new ArraySegment<byte>(responseData, 0, responseData.Length),
+                         WebSocketMessageType.Text, receiveResult.EndOfMessage, CancellationToken.None);
+
+                    var outputData = new ArraySegment<byte>(buffer);
+                    
+                    receiveResult = await webSocket.ReceiveAsync(outputData, CancellationToken.None);
+                }
+
+                await webSocket.CloseAsync(
+                    receiveResult.CloseStatus.Value,
+                    receiveResult.CloseStatusDescription,
+                    CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                var asd = ex.Message;
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ TestWebSocketApiIIko/Services/ResponseService.cs
@@ -0,0 +1,226 @@
+﻿using Newtonsoft.Json;
+
+namespace TestWebSocketApiIIko.Services
+{
+    public class ResponseService
+    {
+        public string Login(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["command"] = "Login";
+            response["status"] = payload.ContainsKey("login") && payload["login"].ToString() == "admin" ? "Success" : "Incorrect login";
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string Sale(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["id"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "Sale";
+
+            response["payload"] = new Dictionary<string, object>
+        {
+            {"type", "Sale"},
+            {"saleId", "019207210000"}
+        };
+
+            if (payload.ContainsKey("payments"))
+            {
+                var payments = new List<Dictionary<string, object>>();
+
+                var asd = ((payload["payments"] as dynamic)[0] as dynamic)["amount"];
+                var payment = new Dictionary<string, object>
+            {
+                {"amount", ((payload["payments"] as dynamic)[0] as dynamic)["amount"]},
+                {"paymentType",( (payload["payments"]as dynamic)[0] as dynamic)["paymentType"]},
+                {"cardPaymentParams", new Dictionary<string, object>
+                    {
+                        {"responseCode", "65"},
+                        {"merchantId", "00000009"},
+                        {"terminalId", "000000009002119"},
+                        {"authCode", "005746"},
+                        {"stan", "53"},
+                        {"cardType", "MASTER International"},
+                        {"rrn", "000117912425"},
+                        {"transactionNumber", "26"}
+                    }
+                }
+            };
+                payments.Add(payment);
+                (response["payload"] as dynamic)["payments"] = payments;
+            }
+
+          (response["payload"] as dynamic)["saleReceipt"] = new Dictionary<string, object>
+        {
+            {"mName", "Dev Team"},
+            {"mAddress", "Тестовый адрес 1"},
+            {"tin", "303109711"},
+            {"date", "2022-07-21"},
+            {"time", "12:19:09"},
+            {"saleId", "019207210000"},
+            {"zNumber", "56"},
+            {"uName", "Админ Админов"},
+            {"operation", "Продажа"},
+            {"productList", new List<Dictionary<string, object>>
+                {
+                    new Dictionary<string, object>
+                    {
+                        {"name", "тестовый продукт"},
+                        {"amount", "1.0"},
+                        {"price", "5 000.00"},
+                        {"totalPrice", "5 000.00"},
+                        {"vat", "454.55"},
+                        {"vatPercent", "0.1"},
+                        {"mxik", "12341234123412341234123"},
+                        {"commissionTIN", "123123123"},
+                        {"barcode", "4674647484949"},
+                        {"label", "01467464748494921yrtril91"}
+                    }
+                }
+            },
+            {"qr", "https://ofd.soliq.uz/check?t=UZ190912010634&r=1679&c=20220721121909&s=499408544365"},
+            {"cashback", "Вы получили право на “кешбек” в размере 1% от покупки!"}
+        };
+
+            if (payload.ContainsKey("payments"))
+            {
+                (response["payload"] as dynamic)["skPayReceipt"] = new Dictionary<string, object>
+            {
+                {"receiptType", "MPS"},
+                {"merchantName", "UZ"},
+                {"merchantAddress", "UZ"},
+                {"cLess", "БЕСКОНТАКТНАЯ ОПЕРАЦИЯ"},
+                {"transactionNumber", "26"},
+                {"tId", "00000009"},
+                {"mId", "000000009002119"},
+                {"softName", "Uzcard POS v1.2.0"},
+                {"sn", "1170393019"},
+                {"rrn", "000117912425"},
+                {"date", "2022-07-21 12:18:59"},
+                {"cardMask", "************5124"},
+                {"aid", "A0000000041010"},
+                {"authCode", "005746"},
+                {"cardType", "MASTERCARD"},
+                {"tvr", "0000008001"},
+                {"tsi", "0000"},
+                {"aip", "1980"},
+                {"transactionType", "Оплата"},
+                {"transactionAmount", "5 000.00"},
+                {"transactionStatus", "Одобрено"},
+                {"commissionAmount", "0.00"},
+                {"total", "5 000.00"},
+                {"authMethod", "Подпись"},
+                {"responseCode", "000"},
+                {"currency", "UZS"},
+                {"applicationName", "MASTERCARD"}
+            };
+            }
+
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string Refund(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "Refund";
+
+            response["payload"] = new Dictionary<string, object>
+        {
+            {"type", "Refund"},
+            {"saleId", "019207210000"}
+        };
+
+            if (payload.ContainsKey("payments"))
+            {
+                (response["payload"] as dynamic)["payments"] = new List<Dictionary<string, object>>
+            {
+                new Dictionary<string, object>
+                {
+                    {"amount", "500000"},
+                    {"paymentType", "Cash"}
+                }
+            };
+            }
+
+            (response["payload"] as dynamic)["refundReceipt"] = null;
+            (response["payload"] as dynamic)["skPayReceipt"] = null;
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string AddPayment(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "AddPayment";
+
+            response["payload"] = new Dictionary<string, object>();
+
+            if (payload.ContainsKey("payment"))
+            {
+                (response["payload"] as dynamic)["payment"] = new Dictionary<string, object>
+            {
+                {"amount", "500000"},
+                {"paymentType", "Cash"}
+            };
+            }
+
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string XReport(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "PrintXReport";
+
+            response["payload"] = null;
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string ZReport(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "CloseZReport";
+
+            response["payload"] = null;
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string ForceReceiptUpload(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "ForceReceiptUpload";
+
+            response["payload"] = null;
+            return JsonConvert.SerializeObject(response);
+        }
+
+        public string PrintLastReceipt(dynamic payload)
+        {
+            var response = new Dictionary<string, object>();
+            response["requestId"] = Guid.NewGuid().ToString();
+            response["status"] = "Success";
+            response["error"] = "PaperEnded";
+            response["command"] = "PrintLastReceipt";
+
+            response["payload"] = null;
+            return JsonConvert.SerializeObject(response);
+        }
+    }
+}
